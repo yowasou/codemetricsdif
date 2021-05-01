f76fe0050a1ec47490799a54e081d2a04bcb9c16
jun.makita <jun.makita@gmail.com>
2021-05-01 16:40:26 +0900

    1

--- codemetricsdiff/Main.cs
+++ codemetricsdiff/Main.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace codemetricsdiff
 {
@@ -20,12 +21,15 @@ namespace codemetricsdiff
         private void LoadXMLList()
         {
             lstXML.Items.Clear();
+            List<string> lstXmllst = new List<string>();
             foreach(string s in System.IO.Directory.GetFiles(
                 Properties.Settings.Default.metrics_output_path, "*.xml", System.IO.SearchOption.AllDirectories))
             {
-                lstXML.Items.Add(System.IO.Path.GetFileName(s));
+                lstXmllst.Add(System.IO.Path.GetFileName(s));
             }
-            
+            lstXmllst.Sort((a, b) => b.CompareTo(a));
+            lstXML.Items.AddRange(lstXmllst.ToArray());
+
         }
 
         private void toolStripMenuItem1_Click(object sender, EventArgs e)
@@ -45,8 +49,86 @@ namespace codemetricsdiff
 
         private void Diff()
         {
-            throw new NotImplementedException();
+            if (lstXML.SelectedItems == null)
+            {
+                MessageBox.Show("XMLを2つ選択してください");
+                return;
+            }
+            if (lstXML.SelectedItems.Count != 2)
+            {
+                MessageBox.Show("XMLを2つ選択してください");
+                return;
+            }
+            string fromXmlFile = Convert.ToString(lstXML.SelectedItems[0]);
+            string toXmlFile = Convert.ToString(lstXML.SelectedItems[1]);
+            if (fromXmlFile.CompareTo(toXmlFile) > 0)
+            {
+                string temp = fromXmlFile;
+                fromXmlFile = toXmlFile;
+                toXmlFile = temp;
+            }
+            DataTable dt = new DataTable("fromdata");
+            dt.Columns.Add("FileName", typeof(string));
+            dt.Columns["FileName"].Caption = "ファイル名";
+            dt.Columns.Add("MethodName", typeof(string));
+            dt.Columns["MethodName"].Caption = "メソッド名";
+            dt.Columns.Add("CyclomaticComplexity", typeof(int));
+            dt.Columns["CyclomaticComplexity"].Caption = "循環的複雑度";
+            dt.Columns.Add("ExecutableLines", typeof(int));
+            dt.Columns["ExecutableLines"].Caption = "ステップ数";
+
+            //XmlSerializerオブジェクトを作成
+            System.Xml.Serialization.XmlSerializer serializer =
+                new System.Xml.Serialization.XmlSerializer(typeof(xmlMetrics.CodeMetricsReport));
+            //読み込むファイルを開く
+            System.IO.StreamReader sr = new System.IO.StreamReader(
+                Properties.Settings.Default.metrics_output_path + "\\" + fromXmlFile, 
+                new System.Text.UTF8Encoding(false));
+            //XMLファイルから読み込み、逆シリアル化する
+            xmlMetrics.CodeMetricsReport crt = 
+                (xmlMetrics.CodeMetricsReport)serializer.Deserialize(sr);
+            foreach(var n in crt.Targets.Target.Assembly.Namespaces)
+            {
+                foreach(var t in n.Types)
+                {
+                    if (t.Members is null)
+                    {
+                        continue;
+                    }
+                    foreach (var mem in t.Members.Items)
+                    {
+                        if (mem is xmlMetrics.CodeMetricsReportTargetsTargetAssemblyNamespaceNamedTypeMembersMethod)
+                        {
+                            xmlMetrics.CodeMetricsReportTargetsTargetAssemblyNamespaceNamedTypeMembersMethod x
+                                = (xmlMetrics.CodeMetricsReportTargetsTargetAssemblyNamespaceNamedTypeMembersMethod)mem;
+                            DataRow drNew = dt.NewRow();
+                            drNew["FileName"] = x.File;
+                            drNew["MethodName"] = x.Name;
+                            drNew["CyclomaticComplexity"] = GetMatricsValue(x.Metrics, "CyclomaticComplexity");
+                            drNew["ExecutableLines"] = GetMatricsValue(x.Metrics, "ExecutableLines");
+                            dt.Rows.Add(drNew);
+                        }
+                    }
+                }
+            }
+            dgv.DataSource = dt;
+            dgv.Columns[0].HeaderText = "ファイル名";
         }
+
+        protected virtual int GetMatricsValue(xmlMetrics.CodeMetricsReportTargetsTargetAssemblyNamespaceNamedTypeMembersMethodMetric[] xs 
+            , string name)
+        {
+            int ret = 0;
+            foreach(var x in xs)
+            {
+                if (x.Name == name)
+                {
+                    ret = x.Value;
+                }
+            }
+            return ret;
+        }
+
 
         private void diffToolStripMenuItem_Click(object sender, EventArgs e)
         {
