0af9af664bde9c743eaef97db45f61c6b462810d
jun.makita <jun.makita@gmail.com>
2021-05-01 15:46:11 +0900

    1

--- codemetricsdiff/Main.Designer.cs
+++ codemetricsdiff/Main.Designer.cs
@@ -29,30 +29,40 @@ namespace codemetricsdiff
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.lstXML = new System.Windows.Forms.ListBox();
             this.btnDiff = new System.Windows.Forms.Button();
             this.dgv = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.diffToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lstXML
             // 
+            this.lstXML.ContextMenuStrip = this.contextMenuStrip1;
             this.lstXML.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lstXML.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.lstXML.FormattingEnabled = true;
-            this.lstXML.ItemHeight = 12;
+            this.lstXML.ItemHeight = 16;
             this.lstXML.Location = new System.Drawing.Point(8, 8);
             this.lstXML.Name = "lstXML";
-            this.lstXML.Size = new System.Drawing.Size(190, 434);
+            this.lstXML.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lstXML.Size = new System.Drawing.Size(239, 434);
             this.lstXML.TabIndex = 0;
             // 
             // btnDiff
             // 
-            this.btnDiff.Location = new System.Drawing.Point(205, 12);
+            this.btnDiff.Location = new System.Drawing.Point(253, 11);
             this.btnDiff.Name = "btnDiff";
             this.btnDiff.Size = new System.Drawing.Size(75, 23);
             this.btnDiff.TabIndex = 1;
             this.btnDiff.Text = "Diff";
             this.btnDiff.UseVisualStyleBackColor = true;
+            this.btnDiff.Click += new System.EventHandler(this.btnDiff_Click);
             // 
             // dgv
             // 
@@ -60,12 +70,40 @@ namespace codemetricsdiff
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv.Location = new System.Drawing.Point(205, 41);
+            this.dgv.Location = new System.Drawing.Point(253, 41);
             this.dgv.Name = "dgv";
             this.dgv.ReadOnly = true;
             this.dgv.RowTemplate.Height = 21;
-            this.dgv.Size = new System.Drawing.Size(584, 398);
+            this.dgv.Size = new System.Drawing.Size(536, 398);
             this.dgv.TabIndex = 2;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.diffToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 76);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem1.Text = "Reload";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // diffToolStripMenuItem
+            // 
+            this.diffToolStripMenuItem.Name = "diffToolStripMenuItem";
+            this.diffToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.diffToolStripMenuItem.Text = "Diff";
+            this.diffToolStripMenuItem.Click += new System.EventHandler(this.diffToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(177, 6);
             // 
             // CodeMetricsDiff
             // 
@@ -78,7 +116,9 @@ namespace codemetricsdiff
             this.Name = "CodeMetricsDiff";
             this.Padding = new System.Windows.Forms.Padding(8);
             this.Text = "CodeMetricsDiff";
+            this.Activated += new System.EventHandler(this.CodeMetricsDiff_Activated);
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -88,6 +128,10 @@ namespace codemetricsdiff
         private System.Windows.Forms.ListBox lstXML;
         private System.Windows.Forms.Button btnDiff;
         private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem diffToolStripMenuItem;
     }
 }
 
--- codemetricsdiff/Main.cs
+++ codemetricsdiff/Main.cs
@@ -16,5 +16,41 @@ namespace codemetricsdiff
         {
             InitializeComponent();
         }
+
+        private void LoadXMLList()
+        {
+            lstXML.Items.Clear();
+            foreach(string s in System.IO.Directory.GetFiles(
+                Properties.Settings.Default.metrics_output_path, "*.xml", System.IO.SearchOption.AllDirectories))
+            {
+                lstXML.Items.Add(System.IO.Path.GetFileName(s));
+            }
+            
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            LoadXMLList();
+        }
+
+        private void CodeMetricsDiff_Activated(object sender, EventArgs e)
+        {
+            LoadXMLList();
+        }
+
+        private void btnDiff_Click(object sender, EventArgs e)
+        {
+            Diff();
+        }
+
+        private void Diff()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void diffToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Diff();
+        }
     }
 }
